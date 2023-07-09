a5836c38837b3362c14e24d4da45caf27e0bda00
Quinn Horton <106427075+q-horton@users.noreply.github.com>
2023-07-09 16:23:47 +1000

    Added self-centering
    Updated to make the rover center itself when it has walls on both sides that can be used to determine an distance offset from the middle of the cell.

--- Code/main.py
+++ Code/main.py
@@ -1,4 +1,5 @@
 from micromelon import *
+import math
 
 # Constants
 
@@ -22,15 +23,29 @@ def correctBearing():
         offset -= TURN
     return round(offset)
 
+# Determines whether the rover is between two walls, and if so uses the difference in distance to center itself.
+def selfCentreAngle():
+    if IR.readLeft() < CELL_SIZE and IR.readRight() < CELL_SIZE:
+        dist_from_mid = abs(IR.readRight() - IR.readLeft()) / 2
+        side = -1 * (not IR.readRight() > IR.readLeft()) + (IR.readRight() > IR.readLeft())
+        if dist_from_mid == 0:
+            return 0
+        tan_theta = CELL_SIZE / dist_from_mid
+        turn_angle = 90 - (math.atan(tan_theta) * 180 / math.pi)
+        return side * round(turn_angle)
+    return 0
+
 # Removes dead ends from the path
 def cullDeadEnds(path_taken):
     dead_ends = True
     while dead_ends:
         dead_ends = False
         for i in range(len(path_taken)):
+            # Dead end found when the rover had to double back on itself
             if path_taken[i] == BACK:
                 dead_ends = True
                 for j in range(1, i):
+                    # Checks whether the moves are still backtracking
                     if path_taken[i - j] == STRAIGHT and path_taken[i + j] == STRAIGHT:
                         continue
                     elif path_taken[i - j] == RIGHT and path_taken[i + j] == LEFT:
@@ -38,12 +53,14 @@ def cullDeadEnds(path_taken):
                     elif path_taken[i - j] == LEFT and path_taken[i + j] == RIGHT:
                         continue
                     else:
+                        # Replaces the original detour with the correct turn
                         if path_taken[i - j] == LEFT and path_taken[i + j] == LEFT:
                             path_taken[i - j] = STRAIGHT
                         elif path_taken[i - j] == LEFT and path_taken[i + j] == STRAIGHT:
                             path_taken[i - j] = RIGHT
                         elif path_taken[i - j] == STRAIGHT and path_taken[i + j] == LEFT:
                             path_taken[i - j] = RIGHT
+                        # Removes the dead end from the route
                         for _ in range(2 * j):
                             path_taken.pop(i - j + 1)
                         break
@@ -73,23 +90,27 @@ def main():
     # Employs a sort of depth-first search to 'hug the left wall', keeping
     # track of the path taken (flipping lefts and rights)
     while True:
+        # Checks in turn: left, straight, right, and backward turns.
         if IR.readLeft() > CELL_SIZE:
+            Motors.write(0, 0, 0.1)
             Motors.turnDegrees(LEFT * TURN + correctBearing(), MOVE_SPEED)
             Motors.moveDistance(CELL_SIZE, MOVE_SPEED)
             path_taken.append(LEFT)
         elif Ultrasonic.read() > CELL_SIZE:
-            Motors.turnDegrees(STRAIGHT * TURN + correctBearing(), MOVE_SPEED)
+            Motors.turnDegrees(STRAIGHT * TURN + correctBearing() + selfCentreAngle(), MOVE_SPEED)
             Motors.moveDistance(CELL_SIZE, MOVE_SPEED)
             path_taken.append(STRAIGHT)
         elif IR.readRight() > CELL_SIZE:
+            Motors.write(0, 0, 0.1)
             Motors.turnDegrees(RIGHT * TURN + correctBearing(), MOVE_SPEED)
             Motors.moveDistance(CELL_SIZE, MOVE_SPEED)
             path_taken.append(RIGHT)
         else:
-            Motors.turnDegrees(BACK * TURN + correctBearing(), MOVE_SPEED)
+            Motors.write(0, 0, 0.1)
+            Motors.turnDegrees(BACK * TURN, MOVE_SPEED)
+            Motors.turnDegrees(correctBearing() + selfCentreAngle(), MOVE_SPEED)
             Motors.moveDistance(CELL_SIZE, MOVE_SPEED)
             path_taken.append(BACK)
-        Motors.write(0, 0, 0.1)
         if reachedEnd():
             break
     # Print statement helps show the rover is on track
@@ -103,8 +124,13 @@ def main():
     # Follows the path taken to return to start
     for i in return_path:
         Motors.moveDistance(CELL_SIZE, MOVE_SPEED)
-        Motors.turnDegrees(i * TURN + correctBearing(), MOVE_SPEED)
         Motors.write(0, 0, 0.1)
+        centering_angle = 0
+        if i == STRAIGHT:
+            centering_angle = selfCentreAngle()
+        elif i == BACK:
+            centering_angle = -selfCentreAngle()
+        Motors.turnDegrees(i * TURN + correctBearing() + centering_angle, MOVE_SPEED)
     print("Returned to start!")
 
 # Setup of device - not part of the algorithm
